69e5e78efc6cc435df093e8514b1605e6c1751fe
Ariel Fogel <fogeltine@gmail.com>
2023-05-22 13:07:25 +0300

    parallelize the work in Threads

--- word_count.rb
+++ word_count.rb
@@ -6,7 +6,6 @@ require 'debug'
 
 require_relative 'word_count/word_count_job'
 
-total_words = 0
 ARTICLES = [
   'https://en.wikipedia.org/wiki/Concurrency_(computer_science)',
   'https://en.wikipedia.org/wiki/Concurrent_computing',
@@ -18,13 +17,27 @@ ARTICLES = [
   'https://en.wikipedia.org/wiki/Concurrent_Design_Facility',
   'https://en.wikipedia.org/wiki/Concurrence'
 ].freeze
+EXPECTED_TOTAL_WORD_COUNT = 9_770
+
+sequential_word_count = 0
+threaded_word_count = 0
+
 
 Benchmark.bm(20) do |job|
   job.report(:sequential) do
     ARTICLES.each do |article_url|
       word_count = WordCountJob.new(article_url).perform
-      total_words = total_words + word_count
+      sequential_word_count += word_count
     end
   end
-  puts "The total words across all the articles are: #{total_words}"
+  job.report(:naive_threaded) do
+    # IO-bound work
+    article_word_counts = ARTICLES.map do |article_url|
+      Thread.new { WordCountJob.new(article_url).perform }
+    end.each(&:join).map(&:value)
+    # CPU-bound work
+    threaded_word_count = article_word_counts.reduce(:+)
+  end
+  puts "The sequential word count across all the articles are: #{sequential_word_count}"
+  puts "The threaded word count across all the articles are: #{threaded_word_count}"
 end
