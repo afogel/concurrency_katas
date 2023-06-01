8e9954a3ce91d40cf1a4660b23d8d86808b5eb69
Ariel Fogel <fogeltine@gmail.com>
2023-06-01 11:43:00 +0300

    add a basic custom formatter

--- logger_kata/log_adapter.rb
+++ logger_kata/log_adapter.rb
@@ -2,11 +2,24 @@ require 'logger'
 require 'debug'
 
 class ConcurrentLogger < Logger
-  attr_reader :output_channel
-
   def initialize(output_channel:)
     @output_channel = output_channel
     super(output_channel)
+    self.formatter = set_formatter
+  end
+
+  private
+
+  attr_reader :output_channel
+
+  def set_formatter
+    proc do |severity, time, progname, msg|
+      "#{severity} #{time.strftime(datetime_format)}: "
+    end
+  end
+
+  def datetime_format
+    "%m-%d-%y %k:%M:%S:%L"
   end
 end
 
